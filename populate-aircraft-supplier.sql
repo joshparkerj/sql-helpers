97b4830b66bf2a74dcc6d1bcd4073d76cd483d29
joshparkerj <superfakek@gmail.com>
2019-05-19 20:31:33 -0500

    populate aircraft supplier

---
+++ populate-aircraft-supplier.sql
@@ -0,0 +1,17 @@
+INSERT INTO aircraft
+(id, manufacturer, name, type, range,
+ speed, load, passengers, engine_power,
+ engine_manufacturer, engine_model,
+ num_engines, length, height,
+ wing_span, ceiling) VALUES
+(29, 'Douglas', 'DC-3', 'Piston', 1299, 180, 8335, 28, '2400 HP', 'Pratt and Whitney', 'PW R-1830', 2, 19.7, 5.16, 29.0, 23200),
+(30, 'Douglas', 'DC-10', 'TurboFan', 5406, 530, 55600, 270, '51000 LBS', 'General Electric', 'GE CF6-50C', 3, 55.5, 17.53, 50.4, 42000),
+(31, 'Aérospatiale', 'Concorde', 'TurboJet', 3900, 1177, 234500, 120, '128000 LBS', 'Rolls-Royce', 'Snecma Olympus 593 Mk 610', 4, 61.67, 12.19, 25.6, 60000),
+(32, 'Douglas', 'DC-9-32', 'TurboFan', 1500, 485, 56325, 139, '36000 LBS', 'Pratt and Whitney', 'JT8D-1', 2, 36.36, 8.5, 28.44, 35000),
+(33, 'Douglas', 'MD-11', 'TurboFan', 7144, 519, 344510, 323, '180000 LBS', 'Pratt and Whitney', 'PW4460', 3, 61.2, 17.6, 51.6, 32600),
+(34, 'Fokker', 'F28 Fellowship', 'TurboFan', 1481, 464, 36006, 85, '19700 LBS', 'Rolls-Royce', 'RB.183 Spey Mk.555-15P', 2, 27.4, 8.47, 25.07, 35000),
+(35, 'Mitsubishi', 'MRJ', 'TurboFan', 2020, 447, 37038, 80, '31200', 'Pratt and Whitney', 'PW1215G', 2, 33.4, 10.4, 29.2, 39000),
+(36, 'Embraer', 'E175', 'TurboFan', 2200, 430, 22289, 76, '28400', 'General Electric', 'CF34', 2, 29.9, 9.85, 26, 41000),
+(37, 'Ilyushin', 'Il-62', 'TurboFan', 5400, 486, 205912, 186, '97000 LBS', 'Solviev', 'D-30', 4, 53.12, 12.35, 43.2, 39400),
+(38, 'Bombardier', 'CRJ900', 'TurboFan', 1553, 447, 22590, 90, '29020 LBS', 'General Electric', 'CF34', 2, 36.2, 7.5, 24.9, 41000),
+(39, 'Bombardier', 'Dash 8', 'TurboProp', 924, 287, 13500, 50, '5000 HP', 'Pratt and Whitney', 'PW100', 2, 25.7, 7.49, 27.4, 25000);
